d1ee6448b7e2dd018d2cad3be33f9608b60857d3
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-06-29 05:09:42 +0200

    Add files via upload

---
+++ hbr.cpp
@@ -0,0 +1,59 @@
+#include "Arme.hpp"
+
+using namespace std;
+
+void Arme(string m_nom="S16",int m_degats=10){
+
+}
+
+ void Armechanger (string nom,int degats)
+{
+  string m_nom=nom;
+  int m_degats=degats;
+}
+
+void Armeafficher(string nom,int degats)
+{
+  string m_nom=nom;
+  int m_degats= degats;
+  
+  std::cout <<"Arme:"<<m_nom<<"degats"<<m_degats<<std::endl;
+}
+
+class Arme(){
+
+  public:
+  void changer(std::string nom,int nbreDetirs)
+  {
+
+  }
+  void afficher (nom,nbreDetirs){
+
+    
+  }
+  void personnageafficherEtat()const{
+
+    std::cout<< vie<<m_vie<<endl;
+
+    m_Arme.afficher;
+  }
+
+  
+  private:
+  std::string m_nom;
+  int m_nbreDetirs;
+};
+
+
+int main()
+{
+/*Creation des Personnages */
+  
+  string Tim,luck("S16",10);
+
+  /* Au combat */
+
+  /* Tim.Armechanger(S56); */
+
+  return 0;
+}
