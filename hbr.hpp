c372bb3970cb375f6500afc87909e36bd28e799e
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-06-29 05:07:23 +0200

    Add files via upload

---
+++ hbr.hpp
@@ -0,0 +1,25 @@
+#ifndef DEF_Arme
+#define DEF_Arme
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Arme(){
+
+  public:
+  void changer(std::string nom,int nbreDetirs){
+
+    
+  }
+  void afficher(nom,nbreDetirs){
+
+    
+  }
+  private:
+  std::string m_nom;/* da initialisiert man die da nicht  */
+  int m_nbreDetirs;
+};
+#endef;
