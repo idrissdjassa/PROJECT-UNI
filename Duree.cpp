45432785e5ded69d56effc6684657a10667579da
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-06-29 05:29:02 +0200

    Add files via upload

---
+++ Duree.cpp
@@ -0,0 +1,16 @@
+#include "Duree.hpp"
+using namespace std;
+
+
+  
+Duree::Duree(int heures,int minutes,int secondes):m_heures(heures),
+m_minutes(minutes),m_secondes(secondes){
+
+}
+
+int main(){
+
+  Duree duree1(O,1O,28),duree2(O,15,2);
+
+  return 0 ;
+}
