12f1be9c826ad04938b51821178615955d76c9f1
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-07-24 18:32:32 +0200

    Create pct2.cpp

---
+++ pct2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include<string>
+#include <vector>
+#include <algorithme>
+#include <cmath>
+#inlcude <map>
+
+using namespace std;
+
+int main(){
+
+    string machaine1("Bonjour !");
+    string machaine2("commennt allez vous")
+    string machaine3("Bien j'espere ")
+    string machaine5("Gloire a Dieu")
+
+    machaine4=machaine1+machaine2+machaine3;
+    std::cout<<machaine4<<std::endl;
+
+    if(machaine1 <= machaine3 ) {
+        std::cout<< machaine1 est inferieur ou egal a  machaine3 <<std::endl;
+        }
+        else {
+            machaine1 est superieur ou egal a machaine3<<std::endl;
+        }
+        std::cout<<"longeur de la chaine est de 10"<<machaine1.size()<<std::endl;
+        
+        machaine2.erase();
+        std::cout<<machaine2<<std::endl;
+        machaine3.clear();
+        std::cout<<machaine3<<std::endl;
+        machaine1.push_back();
+        st::cout<<machaine1<<std::endl;
+
+        std::cout<<machaine5.substr(4)<<endl;
+
+        int a(3,)b(4);
+    if(a==b){
+        std::cout <<"equal"<<std::endl;
+    }
+    if(operatoor==(a,b)){
+        std::cout<<"equal"<<std::endl;
+    }
+}
+
+
+if (duree1==duree2)
+{
+    std::cout <<"les durees sont egales !"<<std::endl;
+
+    
+    
+    return 0;
+
+}
