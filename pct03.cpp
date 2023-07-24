f49355af94afcbb0eb0948ddd299ea92550a62e4
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-07-24 19:59:04 +0200

    Create pct03.cpp

---
+++ pct03.cpp
@@ -0,0 +1,63 @@
+#include "pct03.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+
+    void  Personnage::recevoirDegats(int nbDegats)
+    {
+        
+        Personnage::m_vie -= nbDegats;
+        
+           /*nbre de Degats recu directement par le Personnage*/
+        if (m_vie < 0)/*pour eviter d'avoir une Vie negative */
+        {
+            m_vie = 0; //on met la vie a 0 cela veut dire mort)
+        }
+    }
+    void    Personnage::attaquer(Personnage& cible)
+    {
+        cible.recevoirDegats(m_degatsArme);
+    }
+    
+    void  Personnage::boirePotionDeVie(int quatitepotion) {
+
+        /*     */
+        m_vie += quatitepotion;
+        
+        if (m_vie > 100)
+        {
+            m_vie = 100;
+        }
+    }
+       
+
+    
+    void Personnage::changerArme(string nomNouvelleArme, int degatsNouvelleArme) {
+
+        m_nomArme = nomNouvelleArme;
+        m_degatsArme = degatsNouvelleArme;
+
+    }
+    bool Personnage::estVivant() {
+        return m_vie > 0;
+    }
+    
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
+// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+
+// Tipps für den Einstieg: 
+//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
+//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
+//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
+//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
+//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
