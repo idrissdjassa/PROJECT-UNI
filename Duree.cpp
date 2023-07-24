41949e72fa9a8cc5f48367e876942a539c313f59
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-07-24 18:19:24 +0200

    pct1

--- Duree.cpp
+++ Duree.cpp
@@ -1,16 +1,22 @@
 #include "Duree.hpp"
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
 using namespace std;
 
-
-  
-Duree::Duree(int heures,int minutes,int secondes):m_heures(heures),
-m_minutes(minutes),m_secondes(secondes){
+Duree::Duree(int heures ,int minutes int secondes ):m_heures (heures),m_minutes(minutes),m_secondes(secondes)
+{
 
 }
 
 int main(){
+int Duree,resultat, duree1(0,10,28),duree2(0,15,2);
+resultat =duree1 + duree2;
+std::cout<<resultat<<std::endl;
 
-  Duree duree1(O,1O,28),duree2(O,15,2);
-
-  return 0 ;
+bool opertaor==(Duree const& duree1,Duree const& duree2);
+return 0;
 }
