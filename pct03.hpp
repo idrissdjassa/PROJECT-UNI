13794e0fe49166d163646568e924abede802df62
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-07-27 15:07:08 +0200

    Update pct03.hpp

--- pct03.hpp
+++ pct03.hpp
@@ -7,18 +7,24 @@ class Personnage
 {
 public:
 
-    void recevoirDegats(int nbDegats);
+    void recevoirDegats(int nbDegats); /* Constructeur */
     void attaquer(Personnage& cible);
     void boirePotionDeVie(int quantitePotion);
     void changerArme(std::string nomNouvelleArme, int degatsNouvelleArme);
     bool estVivant();
-
+    void personnage(std::string nomArme, int degatsArme);
+    /* On peut surcharger le Constructeur car ils s'agit d'une Methode */
 private:
 
     int m_vie;
     int m_mana;
     std::string m_nomArme; //Pas de using namespace std, il faut donc mettre std:: devant string
     int m_degatsArme;
+ 
+
+    
 };
+
+~personnage();
 
 #endif
