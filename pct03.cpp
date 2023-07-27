40a926792412f418b4ce92f2cea6493ea54fa489
idrissdjassa <117619499+idrissdjassa@users.noreply.github.com>
2023-07-27 15:07:53 +0200

    Update pct03.cpp

--- pct03.cpp
+++ pct03.cpp
@@ -1,49 +1,76 @@
-#include "pct03.hpp"
+#include "personnage.hpp"
 #include <iostream>
 #include <string>
 using namespace std;
 
+/* Personnage.hpp est le header de classe Personnnage */
+
+/* Un constructeur est apppele pour initialiser les attributs qui ne sont pas des objects */
+
+/*La Methode ne doit rien renvoyer */
 
 
-    void  Personnage::recevoirDegats(int nbDegats)
+personnage::~personnage() {  /* Prototype de Destructeur */
+
+
+
+}
+
+
+personnage::personnage() /* Constructeur */ {
+    m_vie  =  100;
+    m_mama  = 100;
+    m_nomArme  =  "Epee rouillee";
+    m_degatsArme  =  10;
+}
+
+personnage::personnage(std::string nomArme, int degatsArme) {
+    m_vie =       100;
+    m_mama =      100;
+    m_nomArme =    "nomArme";
+    m_degatsArme = "degatsArme";
+}
+
+
+void  Personnage::recevoirDegats(int nbDegats)
+{
+
+    Personnage::m_vie -= nbDegats;
+
+    /*nbre de Degats recu directement par le Personnage*/
+    if (m_vie < 0)/*pour eviter d'avoir une Vie negative */
     {
-        
-        Personnage::m_vie -= nbDegats;
-        
-           /*nbre de Degats recu directement par le Personnage*/
-        if (m_vie < 0)/*pour eviter d'avoir une Vie negative */
-        {
-            m_vie = 0; //on met la vie a 0 cela veut dire mort)
-        }
+        m_vie = 0; //on met la vie a 0 cela veut dire mort)
     }
-    void    Personnage::attaquer(Personnage& cible)
+}
+void    Personnage::attaquer(Personnage& cible)
+{
+    cible.recevoirDegats(m_degatsArme);
+}
+
+void  Personnage::boirePotionDeVie(int quatitepotion) {
+
+    /*     */
+    m_vie += quatitepotion;
+
+    if (m_vie > 100)
     {
-        cible.recevoirDegats(m_degatsArme);
+        m_vie = 100;
     }
-    
-    void  Personnage::boirePotionDeVie(int quatitepotion) {
+}
 
-        /*     */
-        m_vie += quatitepotion;
-        
-        if (m_vie > 100)
-        {
-            m_vie = 100;
-        }
-    }
-       
 
-    
-    void Personnage::changerArme(string nomNouvelleArme, int degatsNouvelleArme) {
 
-        m_nomArme = nomNouvelleArme;
-        m_degatsArme = degatsNouvelleArme;
+void Personnage::changerArme(string nomNouvelleArme, int degatsNouvelleArme) {
 
-    }
-    bool Personnage::estVivant() {
-        return m_vie > 0;
-    }
-    
+    m_nomArme = nomNouvelleArme;
+    m_degatsArme = degatsNouvelleArme;
+
+}
+bool Personnage::estVivant() {
+    return m_vie > 0;
+}
+
 
 
 int main()
@@ -60,4 +87,4 @@ int main()
 //   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
 //   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
 //   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "D
